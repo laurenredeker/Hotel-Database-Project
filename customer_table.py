3909eb62d50f5ae46802db4b637a428634348597
Lauren Redeker <51378187+laurenredeker@users.noreply.github.com>
2019-10-08 22:08:00 -0400

    Create customer_table.py

---
+++ customer_table.py
@@ -0,0 +1,38 @@
+### #!/usr/local/bin/python3    use the absolute path of python interpreter ###
+### should be in the ScriptAlias for the cgi; on mac it's preset to /Library/WebServer/CGI-Executables/
+### change script's permissions in terminal:  sudo chmod 755 customer_table.py ###
+
+print("Content-Type: text/html")
+print()
+
+import pymysql
+import cgi,cgitb
+cgitb.enable() #for debugging
+form = cgi.FieldStorage()
+a = form.getvalue('first_name')
+b = form.getvalue('last_name')
+c = form.getvalue('cid')
+d = form.getvalue('customer_type')
+e = form.getvalue('DOB')
+f = form.getvalue('rewards')
+print("Here is the updated Customers table:")
+
+# Open connection to the database
+db = pymysql.connect("localhost","root","password","project_folder",autocommit=True)
+
+# Start a cursor object using cursor() method
+cursor = db.cursor()
+
+insert = f"""INSERT INTO Customers (first_name,last_name,cid,customer_type,DOB,rewards) VALUES
+('{a}','{b}','{c}','{d}','{e}','{f}')"""
+cursor.execute(insert)
+
+sql_ret = "SELECT * FROM Customers;"
+cursor.execute(sql_ret)
+
+# Fetch all the rows using fetchall() method.
+data = cursor.fetchall()
+print(data)
+
+# disconnect from server
+db.close()
